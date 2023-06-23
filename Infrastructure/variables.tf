ac3bfe08e2734523e3dc9611cd988a7cfdf5b006
get2bash <get2bash@gmail.com>
2023-06-23 17:09:15 +0100

    complete infra

---
+++ Infrastructure/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  type        = string
+  description = "Region"
+  default = "eu-west-2"
+}
+
+variable "av_zone1" {
+  type        = string
+  description = "Availability zone1"
+  default = "eu-west-2a"
+}
+
+variable "av_zone2" {
+  type        = string
+  description = "Availability zone2"
+  default = "eu-west-2b"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type of instance used"
+  default = "t2.micro"
+}
+
+variable "montoring_instance_type" {
+  type        = string
+  description = "Type of instance used for monitoring"
+  default = "t2.medium"
+}
+
+variable "domain_name" {
+  default    = "de-marauder.me"
+  type        = string
+  description = "Domain name"
+}
